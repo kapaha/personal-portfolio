b04432bcff59d0c6ed1939d1214c3759e5bc5d87
Kai Paterson-Hall <kaizwebdesign@gmail.com>
2023-11-12 18:41:29 +0000

    feat: :sparkles: Add Nav menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,57 @@
-import "../css/index.css";
+import "../css/index.scss";
+import "../css/fontawesome.css";
+import "../css/brands.css";
+import "../css/mobile-menu.scss";
 
-const chicken = 1;
+/*
+===============================================================================
+Elements
+===============================================================================
+*/
 
-console.log(chicken);
+const header = document.querySelector(".header");
+const burger = document.querySelector(".hamburger");
+const mobileMenuLinks = document.querySelectorAll(".header  a");
+const mobileMenuToggler = document.querySelectorAll(
+    "[data-mobile-menu-toggler]",
+);
+
+/*
+===============================================================================
+Event Listeners
+===============================================================================
+*/
+
+mobileMenuToggler.forEach((element) =>
+    element.addEventListener("click", toggleMobileMenu),
+);
+
+/*
+===============================================================================
+Helper functions
+===============================================================================
+*/
+
+function toggleMobileMenu() {
+    const shouldClose = header.classList.contains("menu-opened");
+
+    if (shouldClose) {
+        closeMobileMenu();
+    } else {
+        openMobileMenu();
+    }
+}
+
+function closeMobileMenu() {
+    header.classList.remove("menu-opened");
+    burger.classList.remove("is-active");
+
+    mobileMenuLinks.forEach((link) => (link.tabIndex = -1));
+}
+
+function openMobileMenu() {
+    header.classList.add("menu-opened");
+    burger.classList.add("is-active");
+
+    mobileMenuLinks.forEach((link) => (link.tabIndex = 0));
+}
